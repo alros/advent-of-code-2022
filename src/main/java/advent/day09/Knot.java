88e5ff1bfd69a9e662ab82b328a8ea4d5525bd0c
Alberto Rossotto <alberto.rossotto@gmail.com>
2022-12-09 23:36:00 +0100

    Day9 (#1)
    day 9

---
+++ src/main/java/advent/day09/Knot.java
@@ -0,0 +1,33 @@
+package advent.day09;
+
+public class Knot {
+	private Point point;
+	private Knot next;
+
+	public Knot(Point point) {
+		this(point, null);
+	}
+
+	public Knot(Point point, Knot next) {
+		this.point = point;
+		this.next = next;
+	}
+
+	public Knot next() {
+		return next;
+	}
+
+	public Point getPoint() {
+		return point;
+	}
+
+	public void setPoint(Point point) {
+		this.point = point;
+	}
+
+	@Override
+	public String toString() {
+		return "Knot [point=" + point + "]";
+	}
+
+}
---
+++ src/main/java/advent/day09/Status.java
@@ -0,0 +1,15 @@
+package advent.day09;
+
+import java.util.Set;
+
+public record Status(Knot head, Set<Point> visited) {
+
+	public Knot tail() {
+		Knot p=head;
+		while(p.next()!=null) {
+			p=p.next();
+		}
+		return p;
+	}
+}
+
